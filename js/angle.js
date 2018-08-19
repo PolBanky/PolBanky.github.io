22313b205b002d4686ca1d9bf8e5941dc20be3f0
PolBanky <pol.banky@gmail.com>
2018-08-19 08:49:06 +0300

    Sin & Cos & ...

--- js/angle.js
+++ js/angle.js
@@ -5,30 +5,31 @@ var iM   = document.getElementById('IM');
 var iS   = document.getElementById('IS');
 var cl   = document.getElementById('clear');    // Знак градус после децимал инпута  
     cl.onclick = clearAll;  // Клик для очистки всех инпутов
-var showRad = document.getElementById('angl_in_rad_output');// Вывод значения угла в радианах
-var showRadLabel = document.getElementById('in_rad_lbl');   // Лебел вывода значения угла в радианах
+var showRad = document.getElementById('angl_in_rad_output');    // Вывод значения угла в радианах
+var showRadLabel = document.getElementById('in_rad_lbl');       // Лебел вывода значения угла в радианах
+var showSin = document.getElementById('sin_output');
+var showCos = document.getElementById('cos_output');
+var showTg = document.getElementById('tg_output');
+var showCtg = document.getElementById('ctg_output');
 
 
 var txt = "";
 var tmp = "";
-// var numDec = 0;
-var numG = 0;
-var numM = 0;
-var numS = 0;
 
 var numDec = 0;         // Угол с клавы в ДЕСЯТИЧНЫХ град
 var Angle_Sec = 0;      // Угол с клавы в секундах
-var deg = 0;            // Целых градусов в угле
+var numG = 0;           // Целых градусов в угле
 var degSec = 0;         // Целых градусов в угле - в секундах
 var fracSec = 0;        // Дробная часть градуса - в секундах
 var FracMin = 0;        // Дробная часть градуса - в минутах
-var min = 0;            // Целых минут в дробной части
+var numM = 0;           // Целых минут в дробной части
 var minSec = 0;         // Целых минут в дробной части - в секундах
-var sec = 0;            // Секунды - то что осталось
+var numS = 0;           // Секунды - то что осталось
 var deg1 = 0;           // deg в deg-min-sec
 var min1 = 0;           // min в deg-min-sec
 var sec1 = 0;           // sec в deg-min-sec
-var a_dec = 0;          // из deg-min-sec в ДЕСЯТИЧНЫЕ град
+
+var angleInRad = 0;
 
 var i = 0;              // номера стартов функции события
  
@@ -63,7 +64,7 @@ iDec.onkeypress = iS.onkeypress = function(event) {
     console.log("Значение tmp == " + tmp + "; type == " + typeof(tmp) + "; length of string == " + tmp.length);
 switch(this) {
     case iDec:
-        if(+tmp > 359.999) {
+        if(+tmp > 359.999999) {
             I("Угол д.б. менее 360 град.");
             return false; }
         break;    
@@ -122,21 +123,19 @@ iDec.oninput = function(event) {
             clearAll();
             return false; }
     console.log("Значение numDec == " + numDec + "; type == " + typeof(numDec));
-    Angle_Sec = numDec * 3600;   // Угол с клавы в секундах
-    deg = Math.floor(numDec);    // ГРАДУСЫ
-    degSec = deg * 3600;                // ГРАДУСЫ - в секундах
-    fracSec = Angle_Sec - degSec;       // Дробная часть градуса - в секундах
-    fracMin = fracSec / 60;             // Дробная часть градуса - в минутах
-    min = Math.floor(fracMin);          // МИНУТЫ
-    minSec = min * 60;                  // МИНУТЫ - в секундах
-    sec = fracSec - minSec;             // СЕКУНДЫ        
-    iG.value = deg;
-    numG = deg;
+    Angle_Sec = numDec * 3600;      // Угол с клавы в секундах
+    numG = Math.floor(numDec);      // ГРАДУСЫ
+    degSec = numG * 3600;           // ГРАДУСЫ - в секундах
+    fracSec = Angle_Sec - degSec;   // Дробная часть градуса - в секундах
+    fracMin = fracSec / 60;         // Дробная часть градуса - в минутах
+    numM = Math.floor(fracMin);     // МИНУТЫ
+    minSec = numM * 60;             // МИНУТЫ - в секундах
+    numS = fracSec - minSec;        // СЕКУНДЫ        
+    iG.value = numG;
     console.log("Значение iG.value == " + iG.value + "; type == " + typeof(iG.value));
-    iM.value = min;
-    numM = min;
-    iS.value = sec.toFixed(4);
-    numS  = sec.toFixed(4);
+    iM.value = numM;
+    numS  = numS.toFixed(4);
+    iS.value = numS;
     }
     I("Ready");
 } // iDec.oninput
@@ -148,31 +147,28 @@ iG.oninput = iM.oninput = function(event) {
     switch(this) {
         case iG:
             if(this.value=="") numG = 0;
-            else numG = parseFloat(this.value);
+            else numG = parseInt(this.value);
             if(numG > 359) {
                 I("Угол д.б. менее 360 град.");
                 this.value = ""; numG = 0;
                 return false; }
-            console.log("numG == " + numG + "; type == " + typeof(numG));
+            console.log("Значение numG == " + numG + "; type == " + typeof(numG));
             break;
         case iM:
             if(this.value=="") numM = 0;
-            else numM = parseFloat(this.value);
+            else numM = parseInt(this.value);
             if(numM > 59) {
                 I("В градусе д.б. менее 60 минут");
                 this.value = ""; numM = 0;
                 return false; }
-            console.log("numM == " + numM + "; type == " + typeof(numM));
+            console.log("Значение numM == " + numM + "; type == " + typeof(numM));
             break;    
         default:
             break;
     }   // switch (this)
     if((numG==0)&(numM==0)&(numS==0))
     { iDec.value = ""; numDec = 0; }
-    else {            
-    console.log("Значение numG == " + numG + "; type == " + typeof(numG));
-    console.log("Значение numM == " + numM + "; type == " + typeof(numM));
-    console.log("Значение numS == " + numS + "; type == " + typeof(numS));
+    else {    
     Solution();
     }
         I("Ready");
@@ -198,19 +194,13 @@ iS.oninput = function(event) {
 
 
 function Solution() {
-    a_dec = 0;
-    deg1 = numG;
-    min1 = numM;
-    sec1 = numS;
-        
-    deg1 = deg1 * 3600;
-    min1 = min1 * 60;
-    sec1 = sec1 * 1;
+    deg1 = numG * 3600;
+    min1 = numM * 60;
+    sec1 = numS * 1;
     deg1 = deg1 + min1 + sec1;
-    a_dec = deg1 / 3600;
-
-    iDec.value = a_dec;
-} // function Event_InputGradMinSec()
+    numDec = deg1 / 3600;
+    iDec.value = numDec;
+} // function Solution()
 
 
 // *************************************
@@ -221,20 +211,49 @@ btn_calc.onclick = function(event) {
     console.log("\nEvent " + ++i + " => " + event.type + " in id == " + event.target.id);
     console.log("Event Button 'Calculate Angle's Trig' = " + event.type + " on " + event.currentTarget.id);
     console.log("X = " + event.clientX + " Y = " + event.clientY);
-    Trig();
+    gradInRad();
+    sin();
+    cos();
+    tg();
+    ctg();
 }
 
-// function Event_ClickBtnTest() {
-//     console.log("\nEvent Button 'Test' = " + event.type + " on " + event.currentTarget.id);
-//     console.log("X = " + event.clientX + " Y = " + event.clientY);
-// }
 
-
-// Событие - клик по кнопке
-function Trig() {
+function gradInRad() {
     var dFactor = Math.PI / 180;  // = 0,01745329251994329576923690768489
-    console.log("\nЗначение numDec for Trig() == " + numDec + "; type == " + typeof(numDec));
-    var Angle_Rad = numDec * dFactor;
-    showRad.innerHTML = Angle_Rad;
+    console.log("\nЗначение numDec for gradInRad() == " + numDec + "; type == " + typeof(numDec));
+    angleInRad = numDec * dFactor;
+    showRad.innerHTML = angleInRad.toFixed(6);
     showRadLabel.innerHTML = "Угол " + numDec + "\u00B0 - в радианах";
 } // function Event_PressButton()
+
+
+function sin() {
+    var sin1 = Math.sin(angleInRad).toFixed(6); 
+    showSin.innerHTML = sin1;
+}
+
+
+function cos() {
+    var cos1 = Math.cos(angleInRad).toFixed(6); 
+    showCos.innerHTML = cos1;
+}
+
+
+function tg() {
+    var tg1 = Math.tan(angleInRad).toFixed(6); 
+    showTg.innerHTML = tg1;
+}
+
+
+function ctg() {
+    var ctg1 = (1/Math.tan(angleInRad)).toFixed(6); 
+    showCtg.innerHTML = ctg1;
+}
+
+
+
+
+
+
+
