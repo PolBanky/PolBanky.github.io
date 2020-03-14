8ba7d294dd7dcd356bcc1699f81ce22c2252598c
PolBanky <pol.banky@gmail.com>
2020-03-15 01:12:06 +0300

    v_threadPitch

---
+++ js/thread.js
@@ -0,0 +1,25 @@
+'use strict';
+// window.addEventListener("load",page_onload); // onLoad
+// function page_onload() { // Обработчик события загрузки страницы
+//     let net = window.navigator.onLine;
+//     console.log("\n%c Страница загружена;  Наличие инета = " + net, 'color: limegreen; font-weight: bold; font-size: 1.4em;');  
+// } // function page_onload()
+// ****************************************************************************
+let v_inputDiaEx  = document.getElementById("inputDiaEx"); // HTML Input
+let v_threadPitch = document.getElementById("threadPitch"); // HTML Input
+
+// let v_srcSum  = document.getElementById("srcSum");  // HTML Input
+// let v_cell_10 = document.getElementById("cell_10"); // HTML ячейка таблицы
+// let v_cell_11 = document.getElementById("cell_11"); // HTML ячейка таблицы
+// let v_cell_20 = document.getElementById("cell_20"); // HTML ячейка таблицы
+// let v_cell_21 = document.getElementById("cell_21"); // HTML ячейка таблицы
+// let v_cell_30 = document.getElementById("cell_30"); // HTML ячейка таблицы
+// let v_cell_31 = document.getElementById("cell_31"); // HTML ячейка таблицы
+// let v_sumAll  = document.getElementById("sumAll");  // HTML ячейка таблицы
+// let v_sumProfit = document.getElementById("sumProfit"); // HTML ячейка таблицы
+
+
+function sol() { // call from => function hello_inputIDec() in hello_input.js
+    console.log('function sol(): v_inputDiaEx.decimal = ',v_inputDiaEx.decimal,';  v_threadPitch.decimal = ',v_threadPitch.decimal);
+    
+} // function sol()
