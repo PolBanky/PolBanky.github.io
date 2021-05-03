8a9c9b3d9c4f2b6118e08a77127ecd29d48fac9c
PolBanky <pol.banky@gmail.com>
2021-05-03 13:31:12 +0300

    commit_2021_05_03

--- js/indexRoot.js
+++ js/indexRoot.js
@@ -5,11 +5,10 @@ let net = window.navigator.onLine;
 
 window.addEventListener("load",page_onload);
 let v_menu3 = document.getElementById('menu3'); /* переменная - для разнообразия */
-console.log('Третий слева пункт меню  id = menu3'); 
-console.log(v_menu3); 
+// console.log('Третий слева пункт меню  id = menu3'); 
+// console.log(v_menu3); 
 
 if(!net) {
-// console.log("\nНаличие инета = " + net);
 // document.getElementById("menu3").disabled = true;
 // document.getElementById("menu1").hidden = true;
 // v_menu3.setAttribute('disabled', true);
@@ -21,21 +20,22 @@ document.getElementById("menu02").hidden = true;
 document.getElementById("menu1").hidden = true;
 document.getElementById("menu2").hidden = true;
 v_menu3.hidden = true; /* переменная - для разнообразия */
+document.getElementById("menu4").hidden = true;
 }
 
 function page_onload() { // Обработчик события загрузки страницы
-    console.log("\nStart page loaded !  Наличие инета = " + net);
+    // console.log("\nStart page loaded !  Наличие инета = " + net);
     displayTime();
 }   // page_onload()
 
-// C L O C K
-var elt = document.getElementById("clock");  // Find element with id="clock"
+    // C L O C K
+var elt = document.getElementById("clock");      // Find element with id="clock"
+let now;
 function displayTime() {
-    var now = new Date();                        // Get current time
+    now = new Date();                            // Get current time
     elt.innerHTML = now.toLocaleTimeString();    // Make elt display it
     setTimeout(displayTime, 1000);               // Run again in 1 second
-}
-// C L O C K
+}   // C L O C K
 
 // document.write("<br>window<br><br>");
 // var k=1;
@@ -44,4 +44,3 @@ function displayTime() {
 // // console.log("<b>"+k+"</b>: "+prop+"<br>");
 // k++;
 // } // for
-
